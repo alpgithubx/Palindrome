636453c1c1755739b5037858871e2f3052be3863
Alp Tuna Şahin <118995494+alpgithubx@users.noreply.github.com>
2023-06-29 14:37:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 1000000; i++) {
+                string str = Convert.ToString(i);
+                string rev = "";
+                for (int j = str.Length - 1; j >= 0; j--) {
+                    string sub = str.Substring(j, 1);
+                    rev = rev + sub;
+                }
+                if (rev == str) {
+                    Console.WriteLine($"{str} bir palindrom!");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
